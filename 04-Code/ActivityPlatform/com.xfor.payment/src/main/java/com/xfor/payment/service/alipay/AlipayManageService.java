b42752fe23f79d8f0cfe38ebcca826615c41974c
xforliux <xforstudio@163.com>
2020-05-07 16:03:38 +0800

    202005071603

---
+++ 04-Code/ActivityPlatform/com.xfor.payment/src/main/java/com/xfor/payment/service/alipay/AlipayManageService.java
@@ -0,0 +1,12 @@
+package com.xfor.payment.service.alipay;
+
+import com.xfor.infrastructure.service.BaseService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlipayManageService extends BaseService {
+
+    public AlipayManageService() {
+        super();
+    }
+}
---
+++ 04-Code/ActivityPlatform/com.xfor.payment/src/main/java/com/xfor/payment/service/weixinpay/WeixinpayManageService.java
@@ -0,0 +1,12 @@
+package com.xfor.payment.service.weixinpay;
+
+import com.xfor.infrastructure.service.BaseService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WeixinpayManageService extends BaseService {
+
+    public WeixinpayManageService() {
+        super();
+    }
+}
